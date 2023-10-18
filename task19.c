da7c810b8aa857f4fedc89bdd03e86734ac273f5
Delasie Zudor <delasiezudor@gmail.com>
2023-10-18 20:58:48 +0000

    update

---
+++ task19.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * _strlen - This function returns to the length of a string
+ * @s: Displays the string of whom the  length is to be checked
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - This function performs a lexicogarphic comparison between two strings.
+ * @s1: displays the first string
+ * @s2: displays the second string
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - This Function checks if then needle starts with an haystack
+ * @haystack: displays the string to search
+ * @needle: displays the sub string to be found
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - This function concatenates on two strings
+ * @dest: Displays the destination of a buffer
+ * @src: Displays the source of a buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
