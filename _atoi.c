f019e60119e07a5376764e3e3a78bbffde4a3b57
Dayking-Eugene <>
2023-10-19 02:41:40 -0700

    done

--- _atoi.c
+++ _atoi.c
@@ -1,75 +1,74 @@
 #include "shell.h"
 
 /**
- * interactive - checks if the shell is in interactive mode
- * @info: The structure address
+ * interactive_mode - Checks if the shell is in interactive mode.
+ * @info: The address of the structure.
  *
- * Return: 1 if the shell is in interactive mode, 0 otherwise
+ * Return: 1 if the shell is in interactive mode, 0 otherwise.
  */
-int interactive(info_t *info)
+int interactive_mode(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+    return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if the given character is a delimiter
- * @c: The character to be checked
- * @delim: The delimiter string
- * Return: 1 if 'c' is a delimiter, otherwise return 0
+ * is_delimiter - Checks if the given character is a delimiter.
+ * @c: The character to be checked.
+ * @delim: The delimiter string.
+ * Return: 1 if 'c' is a delimiter, otherwise return 0.
  */
-int is_delim(char c, char *delim)
+int is_delimiter(char c, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+    while (*delim)
+    {
+        if (*delim++ == c)
+            return (1);
+    }
+    return (0);
 }
 
 /**
- * _isalpha - checks if the given character is an alphabetic character
- * @c: The character to be checked
- * Return: 1 if 'c' is an alphabetic character, otherwise return 0
+ * is_alpha - Checks if the given character is an alphabetic character.
+ * @c: The character to be checked.
+ * Return: 1 if 'c' is an alphabetic character, otherwise return 0.
  */
-
-int _isalpha(int c)
+int is_alpha(int c)
 {
-	if ((c >= 'b’' && c <= 'x') || (c >= 'B' && c <= 'X'))
-		return (1);
-	else
-		return (0);
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return (1);
+    else
+        return (0);
 }
 
 /**
- * _atoi - transforms the given string into an integer
- * @s: string to be transformed
- * Return:  if no numbers in string return 0, otherwise converted number
+ * string_to_integer - Transforms the given string into an integer.
+ * @s: The string to be transformed.
+ * Return: If no numbers in string return 0, otherwise returns the converted number.
  */
-
-int _atoi(char *s)
+int string_to_integer(char *s)
 {
-	int j, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+    int j, sign = 1, flag = 0, output;
+    unsigned int result = 0;
 
-	for (j = 0; s[i] != '\0' && flag != 2; j++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
+    for (j = 0; s[j] != '\0' && flag != 2; j++)
+    {
+        if (s[j] == '-')
+            sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
+        if (s[j] >= '0' && s[j] <= '9')
+        {
+            flag = 1;
+            result *= 10;
+            result += (s[j] - '0');
+        }
+        else if (flag == 1)
+            flag = 2;
+    }
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
+    if (sign == -1)
+        output = -result;
+    else
+        output = result;
 
-	return (output);
+    return (output);
 }
-
